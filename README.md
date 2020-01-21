7c8204fa843dd1af91f1550551f7352e7d53f56d
Dildo Bagginz <dylankepley@me.com>
2020-01-21 00:42:56 -0800

    code base

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Your application should have a clean and polished user interface and be responsi
 
 Your application should be deployed to GitHub Pages.
 
-Your application's GitHub repository should contain a README.md file explaining the purpose and functionality of the application. The README.md file should include a screenshot of the completed application as well as a link to the deployed GitHub Pages URL.
+Your application's GitHub repository should contain a README.md file explaining the purpose and functionality of the application. The README.md file should include a screenshot of the completed application as well as a link to the deployehttp://codingheroes.io/rescourcesd GitHub Pages URL.
 
 ![password generator demo](./Assets/03-JavaScript-homework-demo.png)
 
--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-//Generator functions - http://www.net-comber.com/charset.html//
-//i used above website for symbol and numbers key instead of typing out "1", "2", "3",..."a","b","c"...etc. 
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
 
 const randomFunc = {
     lower: getRandomLower,
@@ -7,6 +13,68 @@ const randomFunc = {
     number: getRandomNumber,
     symbol: getRandomSymbol
 };
+
+//Generate Event Listener
+clipboard.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
+});
+// Generate Event Listener//
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value; //plus sign makes lengthEl value a number and not a "string"!!!
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{
+        lower
+    }, {
+        upper
+    }, {
+        number
+    }, {
+        symbol
+    }].filter(item => Object.values(item)[0]);
+
+    //Doesn't have a selected type
+    if (typesCount === 0) {
+        return '';
+    }
+
+    //create loop
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatePassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
+
+
+//Generator functions....used this key- http://www.net-comber.com/charset.html - for numbers code//
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -17,12 +85,10 @@ function getRandomUpper() {
 }
 
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomSymbol())
